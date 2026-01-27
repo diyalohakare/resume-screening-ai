d332caf75f90d2e40e783537aa2ae9df082ec4d0
diyalohakare <diyalohakare4658@gmail.com>
2026-01-27 18:56:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -50,4 +50,10 @@ def home():
 
     return render_template("index.html", result=result, match=match)
 
-app.run(debug=True)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
+
